07453ab512dad2e7c367fa6cae05eb7beda66a5d
Adrian Cooper <adrian.cooper686@gmail.com>
2025-07-28 17:02:12 +0100

    Initial Commit

---
+++ Personal Spending Manager/Personal_Spending_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Spending_ManagerApp.swift
+//  Personal Spending Manager
+//
+//  Created by Adrian Cooper on 28/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Personal_Spending_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Personal Spending ManagerTests/Personal_Spending_ManagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Personal_Spending_ManagerTests.swift
+//  Personal Spending ManagerTests
+//
+//  Created by Adrian Cooper on 28/07/2025.
+//
+
+import Testing
+
+struct Personal_Spending_ManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
